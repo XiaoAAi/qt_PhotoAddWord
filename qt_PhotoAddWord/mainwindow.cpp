a4db4b9e08896b4662ac50ce2cd7091d16776d27
yuBW.com <YBW>
2020-11-17 22:46:48 +0800

    版本号:PhotoAddWord_V1.0.0;Date:2020.11.17+显示图片相对路 径以及基本结构简历

--- qt_PhotoAddWord/mainwindow.cpp
+++ qt_PhotoAddWord/mainwindow.cpp
@@ -1,15 +1,35 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
+#include <QDebug>
+
+#define   VERSION    "PhotoAddWord_V1.0.0;Date:2020.11.17"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle(VERSION);
+
+    QPixmap pix(":/Po/show.jpg");
+    if(pix.isNull()){
+        qDebug("isNUll\n");
+    }
+    pix.scaled(QSize(481, 481), Qt::KeepAspectRatio);
+    ui->lab_preview->setPixmap(pix);
+    ui->lab_preview->setScaledContents(true);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+//功能：btn_input导入图片或者拖拽图片路径
+void MainWindow::on_btn_input_clicked()
+{
+
 }
 
--- qt_PhotoAddWord/mainwindow.h
+++ qt_PhotoAddWord/mainwindow.h
@@ -15,6 +15,10 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btn_input_clicked();
+
+
 private:
     Ui::MainWindow *ui;
 };
